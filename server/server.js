e677d442ae5b7a171fab6d4e7390046f3ff96df7
Joseph <josefafonne@yahoo.co.uk>
2018-01-29 20:19:36 +0200

    create generateMessage

--- server/server.js
+++ server/server.js
@@ -2,53 +2,30 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+var favicon = require('serve-favicon');
 
+const {generateMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 var app = express();
+
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(favicon(path.join(__dirname,'../public','images','favicon.ico')));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
   // to the sender
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app',
-    createdAt: new Date().toString()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
   // to the other users
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().toString()
-  });
-
-  // socket.emit('newMessage', {
-  //   from: 'kelvin.elechukwu@gmail.com',
-  //   text: 'Hey, how are you doing?',
-  //   createdAt: new Date().toString()
-  // });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   socket.on('createMessage', function(message) {
     console.log('create message', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().toString()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
-  // socket.emit('newEmail', {
-  //   from: 'joseph@gmail.com',
-  //   message: 'Message from Joseph',
-  //   received: new Date().toString()
-  // });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
