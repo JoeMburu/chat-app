be2330685a36ca595a7fcb8d5d4c412bb02f5400
Joseph <josefafonne@yahoo.co.uk>
2018-01-31 19:02:42 +0200

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,26 +2,42 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.on('newMessage', function(newMessage) {
-    console.log('Got new message', newMessage);
-  });
-
-  // socket.emit('createEmail', {
-  //   to: 'jen@example.com',
-  //   text: 'Hey. This is Andrew.'
-  // });
 });
 
-// socket.emit('createMessage', {
-//   from: 'chika@gmail.com',
-//   text: 'Man pass man'
-// });
+socket.on('newMessage', function(message) {
+  console.log('Got new message', message);
+
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
 
 socket.on('disconnect', function() {
   console.log('Disconnected from the server');
 });
 
-// socket.on('newEmail', function(email) {
-//   console.log('New email', email);
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// }, function(data) {
+//   console.log('Got it.', data);
+// });
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function() {
+
+  });
+
+});
+
+
+// $(function() {
+//
+//
 // });
--- server/server.js
+++ server/server.js
@@ -22,9 +22,10 @@ io.on('connection', (socket) => {
   // to the other users
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', function(message) {
+  socket.on('createMessage', function(message, callback) {
     console.log('create message', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
   });
 
   socket.on('disconnect', () => {
